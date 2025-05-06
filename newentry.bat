0356dfddb84ad2e843d5e7b6699dab020f5687bb
MohammadYehya <mohammad7446@gmail.com>
2025-05-06 22:57:03 +0500

    Create newentry.bat

---
+++ newentry.bat
@@ -0,0 +1,9 @@
+@echo off
+@REM To use just go into main folder (i.e. leetcode) and run the command with a name for the new folder (i.e. 00002. Add Two Numbers)
+
+mkdir %1
+cd %1
+mkdir C++
+mkdir Python
+mkdir Rust
+echo( > Readme.md
