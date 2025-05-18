b771e5ac91b1837069a387674a922cc627d14d09
MohammadYehya <mohammad7446@gmail.com>
2025-05-18 17:23:09 +0500

    Update sol.py

--- leetcode/00001. Two Sum/Python/sol.py
+++ leetcode/00001. Two Sum/Python/sol.py
@@ -1,1 +1,18 @@
- 
+# Bruteforce Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                
+# Hashmap Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i, n in enumerate(nums):
+            valy = target - n
+            if valy in hashmap:
+                return [hashmap[valy], i]
+            hashmap[n] = i
+        return []
