8d29e2f985b7730415ed6b7456723eb229406069
MohammadYehya <mohammad7446@gmail.com>
2025-05-21 18:44:56 +0500

    Create sol.rs

---
+++ leetcode/00009. Palindrome Number/Rust/sol.rs
@@ -0,0 +1,16 @@
+// Integer Reversing Technique
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        if (x < 0) {
+            return false;
+        }
+        let mut num = x;
+        let mut rev = 0;
+        while (num > 0)
+        {
+            rev = rev*10 + num%10;
+            num = num/10;
+        }
+        rev == x
+    }
+}
