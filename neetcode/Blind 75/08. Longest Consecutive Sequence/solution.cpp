fc3efacf951e4125bd12d803ca512dd3db4aafbb
Mohammad Yehya <56927485+MohammadYehya@users.noreply.github.com>
2026-04-14 11:27:37 +0500

    B75-8

---
+++ neetcode/Blind 75/08. Longest Consecutive Sequence/solution.cpp
@@ -0,0 +1,37 @@
+//Hashset Solution
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        int longest = 0;
+
+        for (int num : numSet) {
+            if (numSet.find(num - 1) == numSet.end()) {
+                int length = 1;
+                while (numSet.find(num + length) != numSet.end()) {
+                    length++;
+                }
+                longest = max(longest, length);
+            }
+        }
+        return longest;
+    }
+};
+
+// Hashmap Solution
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        int res = 0;
+        for (int num : nums) {
+            if (!mp[num]) {
+                mp[num] = mp[num - 1] + mp[num + 1] + 1;
+                mp[num - mp[num - 1]] = mp[num];
+                mp[num + mp[num + 1]] = mp[num];
+                res = max(res, mp[num]);
+            }
+        }
+        return res;
+    }
+};
